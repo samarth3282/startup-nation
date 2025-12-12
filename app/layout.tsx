437c31006a2cb65d3743174fa6aa019f4cfc15c1
samarthunflagged <samarthnimeshkumarpatel@gmail.com>
2025-12-12 22:59:30 +0530

    fix prettier errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,36 +4,36 @@ import "./globals.css";
 import localFont from "next/font/local";
 
 const workSans = localFont({
-  src:[
+  src: [
     {
-      path:'./fonts/WorkSans-Black.ttf',
-      weight:'900',
-      style:'normal'
+      path: "./fonts/WorkSans-Black.ttf",
+      weight: "900",
+      style: "normal",
     },
     {
-      path:'./fonts/WorkSans-ExtraBold.ttf',
-      weight:'800',
-      style:'normal'
+      path: "./fonts/WorkSans-ExtraBold.ttf",
+      weight: "800",
+      style: "normal",
     },
     {
-      path:'./fonts/WorkSans-Bold.ttf',
-      weight:'700',
-      style:'normal'
+      path: "./fonts/WorkSans-Bold.ttf",
+      weight: "700",
+      style: "normal",
     },
     {
-      path:'./fonts/WorkSans-SemiBold.ttf',
-      weight:'600',
-      style:'normal'
+      path: "./fonts/WorkSans-SemiBold.ttf",
+      weight: "600",
+      style: "normal",
     },
     {
-      path:'./fonts/WorkSans-Medium.ttf',
-      weight:'500',
-      style:'normal'
+      path: "./fonts/WorkSans-Medium.ttf",
+      weight: "500",
+      style: "normal",
     },
     {
-      path:'./fonts/WorkSans-Regular.ttf',
-      weight:'400',
-      style:'normal'
+      path: "./fonts/WorkSans-Regular.ttf",
+      weight: "400",
+      style: "normal",
     },
     // {
     //   path:'./fonts/WorkSans-Medium.ttf',
@@ -41,18 +41,18 @@ const workSans = localFont({
     //   style:'normal'
     // },
     {
-      path:'./fonts/WorkSans-Thin.ttf',
-      weight:'200',
-      style:'normal'
+      path: "./fonts/WorkSans-Thin.ttf",
+      weight: "200",
+      style: "normal",
     },
     {
-      path:'./fonts/WorkSans-ExtraLight.ttf',
-      weight:'100',
-      style:'normal'
+      path: "./fonts/WorkSans-ExtraLight.ttf",
+      weight: "100",
+      style: "normal",
     },
   ],
-  variable:'--font-work-sans',
-})
+  variable: "--font-work-sans",
+});
 
 export const metadata: Metadata = {
   title: "YC Directory",
@@ -66,11 +66,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={workSans.variable}
-      >
-        {children}
-      </body>
+      <body className={workSans.variable}>{children}</body>
     </html>
   );
 }
